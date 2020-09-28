48ae80e1ac968b33579dd4a03ac370815eaff900
Mark <markdavies550@gmail.com>
2020-09-28 21:54:48 +0100

    test

--- src/Tweeter/Startup.cs
+++ src/Tweeter/Startup.cs
@@ -26,6 +26,7 @@ namespace Tweeter
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
